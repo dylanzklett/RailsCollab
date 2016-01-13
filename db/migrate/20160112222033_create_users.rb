1596f8fa444a7bd57f5d761d8cfd4aad04b287ff
Akeem Spencer <aspencerpsu@gmail.com>
2016-01-13 12:55:35 -0500

    Remove the schema and the migration files to start over again

--- db/migrate/20160112222033_create_users.rb
+++ db/migrate/20160112222033_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+    	t.string :username
+    	t.password :password
+
+
 
       t.timestamps null: false
     end
